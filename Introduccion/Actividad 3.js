ef5d4db99b0c4a48344339e5527ec5b628e569e2
Omar Melgar <36140646+omarmc90@users.noreply.github.com>
2018-02-12 23:04:25 -0600

    Create Actividad 3.js
    Código solicitado en la Actividad 3 - JavaScript

---
+++ Introduccion/Actividad 3.js
@@ -0,0 +1,39 @@
+var mes = 10
+if (mes == 1) {
+    alert("Es Invierno"), alert("Omar Alberto Melgar Cabrera - Carné 16007108");
+    console.log("Enero")
+} else if (mes == 2) {
+ alert("Es Invierno"), alert("Omar Alberto Melgar Cabrera - Carné 16007108");
+    console.log("Febrero")
+} else if (mes == 12) {
+ alert("Es Invierno"), alert("Omar Alberto Melgar Cabrera - Carné 16007108");
+    console.log("Diciembre")
+} else if (mes == 3) {
+ alert("Es Primavera"), alert("Omar Alberto Melgar Cabrera - Carné 16007108");
+    console.log("Marzo")
+} else if (mes == 4) {
+ alert("Es Primavera"), alert("Omar Alberto Melgar Cabrera - Carné 16007108");
+    console.log("Abril")
+} else if (mes == 5) {
+ alert("Es Primavera"), alert("Omar Alberto Melgar Cabrera - Carné 16007108");
+    console.log("Mayo")
+} else if (mes == 6) {
+ alert("Es Verano"), alert("Omar Alberto Melgar Cabrera - Carné 16007108");
+    console.log("Junio")
+} else if (mes == 7){
+ alert("Es Verano"), alert("Omar Alberto Melgar Cabrera - Carné 16007108");
+    console.log("Julio")
+} else if (mes == 8) {
+ alert("Es Verano"), alert("Omar Alberto Melgar Cabrera - Carné 16007108");
+    console.log("Agosto")
+} else if (mes == 9) {
+ alert("Es Otoño"), alert("Omar Alberto Melgar Cabrera - Carné 16007108");
+    console.log("Septiembre")
+} else if (mes == 10) {
+ alert("Es Otoño"), alert("Omar Alberto Melgar Cabrera - Carné 16007108");
+    console.log("Octubre")
+} else if (mes == 11) {
+ alert("Es Otoño"), alert("Omar Alberto Melgar Cabrera - Carné 16007108");
+    console.log("Noviembre")
+} else {alert("El valor ingresado no es válido");
+}
